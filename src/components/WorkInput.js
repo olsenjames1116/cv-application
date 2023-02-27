eb6271653fec889376ecec1348fbf95d0b198703
James Olsen <olsenjames1116@gmail.com>
2023-02-26 17:15:44 -0800

    Update state when work input changes

--- src/components/WorkInput.js
+++ src/components/WorkInput.js
@@ -1,21 +1,25 @@
 import React from "react";
 
 export default class WorkInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <fieldset>
                 <legend>Work Experience</legend>
                 <div>
                     <label htmlFor='company'>Company:</label>
-                    <input type='text' id='company'></input>
+                    <input type='text' id='company' onChange={this.props.onInputChange}></input>
                     <label htmlFor='workPosition'>Position:</label>
-                    <input type='text' id='workPosition'></input>
+                    <input type='text' id='workPosition' onChange={this.props.onInputChange}></input>
                     <label htmlFor='workStart'>Start Date:</label>
-                    <input type='date' id='workStart'></input>
+                    <input type='date' id='workStart' onChange={this.props.onInputChange}></input>
                     <label htmlFor='workEnd'>End Date:</label>
-                    <input type='date' id='workEnd'></input>
+                    <input type='date' id='workEnd' onChange={this.props.onInputChange}></input>
                     <label htmlFor='workDescription'>Description:</label>
-                    <textarea id='workDescription'></textarea> 
+                    <textarea id='workDescription' onChange={this.props.onInputChange}></textarea> 
                 </div>
             </fieldset>
         );
