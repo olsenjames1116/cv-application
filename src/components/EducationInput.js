4a0728e5e7c7e0af64d572a3ce692a70ed4f831b
James Olsen <olsenjames1116@gmail.com>
2023-02-26 17:21:40 -0800

    Update state when education input changes

--- src/components/EducationInput.js
+++ src/components/EducationInput.js
@@ -1,21 +1,25 @@
 import React from "react";
 
 export default class EducationInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <fieldset>
                 <legend>Education</legend>
                 <div>
                     <label htmlFor='curriculum'>Curriculum:</label>
-                    <input type='text' id='curriculum'></input>
+                    <input type='text' id='curriculum' onChange={this.props.onInputChange}></input>
                     <label htmlFor='school'>School:</label>
-                    <input type='text' id='school'></input>
+                    <input type='text' id='school' onChange={this.props.onInputChange}></input>
                     <label htmlFor='schoolStart'>Start Date:</label>
-                    <input type='date' id='schoolStart'></input>
+                    <input type='date' id='schoolStart' onChange={this.props.onInputChange}></input>
                     <label htmlFor='schoolEnd'>End Date:</label>
-                    <input type='date' id='schoolEnd'></input>
+                    <input type='date' id='schoolEnd' onChange={this.props.onInputChange}></input>
                     <label htmlFor='schoolDescription'>Description:</label>
-                    <textarea id='schoolDescription'></textarea>
+                    <textarea id='schoolDescription' onChange={this.props.onInputChange}></textarea>
                 </div>
             </fieldset>
         );
