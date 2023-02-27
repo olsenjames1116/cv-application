77cf68e16872332ed06bb3712619029f9664ca02
James Olsen <olsenjames1116@gmail.com>
2023-02-26 17:13:31 -0800

    Update state when skills input changes

--- src/components/SkillsInput.js
+++ src/components/SkillsInput.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 export default class SkillsInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <div>
                 <label htmlFor='skills'>Skills:</label>
-                <textarea id='skills'></textarea>
+                <textarea id='skills' onChange={this.props.onInputChange}></textarea>
             </div>
         );
     }
