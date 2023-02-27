ff85a7af54e88988636c06cd8d9f462b16075e6e
James Olsen <olsenjames1116@gmail.com>
2023-02-26 17:23:35 -0800

    Update state when misc input changes

--- src/components/MiscInput.js
+++ src/components/MiscInput.js
@@ -1,21 +1,25 @@
 import React from "react";
 
 export default class MiscInput extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <fieldset>
                 <legend>Misc Experience</legend>
                 <div>
                     <label htmlFor='organization'>Organization:</label>
-                    <input type='text' id='organization'></input>
+                    <input type='text' id='organization' onChange={this.props.onInputChange}></input>
                     <label htmlFor='miscPosition'>Position:</label>
-                    <input type='text' id='miscPosition'></input>
+                    <input type='text' id='miscPosition' onChange={this.props.onInputChange}></input>
                     <label htmlFor='miscStart'>Start Date:</label>
-                    <input type='date' id='miscStart'></input>
+                    <input type='date' id='miscStart' onChange={this.props.onInputChange}></input>
                     <label htmlFor='miscEnd'>End Date:</label>
-                    <input type='date' id='miscEnd'></input>
+                    <input type='date' id='miscEnd' onChange={this.props.onInputChange}></input>
                     <label htmlFor='miscDescription'>Description:</label>
-                    <textarea id='miscDescription'></textarea> 
+                    <textarea id='miscDescription' onChange={this.props.onInputChange}></textarea> 
                 </div>
             </fieldset>
         );
