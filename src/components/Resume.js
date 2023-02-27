8c58d06e6af8417b642f4d80f9b6972027b526a6
James Olsen <olsenjames1116@gmail.com>
2023-02-26 16:11:45 -0800

    Dummy text displayed for work experience on resume

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -2,6 +2,7 @@ import React from "react";
 import Contact from './Contact';
 import Summary from './Summary';
 import Skills from './Skills';
+import Work from './Work';
 
 export default class Resume extends React.Component {
     render() {
@@ -10,6 +11,7 @@ export default class Resume extends React.Component {
                 <Contact />
                 <Summary />
                 <Skills />
+                <Work />
             </div>
         );
     }
--- src/components/Work.js
+++ src/components/Work.js
@@ -1,5 +1,12 @@
 import React from 'react';
 
 export default class Work extends React.Component {
-    
+    render() {
+        return (
+            <div>
+                <h2>Work Experience</h2>
+                <p>lorem</p>
+            </div>
+        );
+    }
 }
