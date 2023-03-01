dde867a828a2ad306c478a382e06c76f3453b6b3
James Olsen <olsenjames1116@gmail.com>
2023-02-28 16:27:05 -0800

    Placeholders for misc experience

--- src/components/MiscInput.js
+++ src/components/MiscInput.js
@@ -17,15 +17,15 @@ export default class MiscInput extends React.Component {
                             return (
                                 <li key={experience.id}>
                                     <label htmlFor={`organization_${experience.id}`}>Organization:</label>
-                                    <input type='text' id={`organization_${experience.id}`} className='misc' onChange={handleChange}></input>
+                                    <input type='text' id={`organization_${experience.id}`} className='misc' placeholder={experience.organization} onChange={handleChange}></input>
                                     <label htmlFor={`position_${experience.id}`}>Position:</label>
-                                    <input type='text' id={`position_${experience.id}`} className='misc' onChange={handleChange}></input>
+                                    <input type='text' id={`position_${experience.id}`} className='misc' placeholder={experience.position} onChange={handleChange}></input>
                                     <label htmlFor={`start_${experience.id}`}>Start Date:</label>
-                                    <input type='date' id={`start_${experience.id}`} className='misc' onChange={handleChange}></input>
+                                    <input type='date' id={`start_${experience.id}`} className='misc' placeholder={experience.start} onChange={handleChange}></input>
                                     <label htmlFor={`end_${experience.id}`}>End Date:</label>
-                                    <input type='date' id={`end_${experience.id}`} className='misc' onChange={handleChange}></input>
+                                    <input type='date' id={`end_${experience.id}`} className='misc' placeholder={experience.end} onChange={handleChange}></input>
                                     <label htmlFor={`description_${experience.id}`}>Description:</label>
-                                    <textarea id={`description_${experience.id}`} className='misc' onChange={handleChange}></textarea>
+                                    <textarea id={`description_${experience.id}`} className='misc' placeholder={experience.description} onChange={handleChange}></textarea>
                                 </li>
                             );
                         })
