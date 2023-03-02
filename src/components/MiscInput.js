b6ac08f35d90d2a86cc41191218ad08ab25358c5
James Olsen <olsenjames1116@gmail.com>
2023-03-01 17:11:22 -0800

    Format misc section of form

--- src/components/MiscInput.js
+++ src/components/MiscInput.js
@@ -1,4 +1,5 @@
 import React from "react";
+import '../styles/MiscInput.css';
 
 export default class MiscInput extends React.Component {
     constructor(props) {
@@ -11,21 +12,35 @@ export default class MiscInput extends React.Component {
         return (
             <fieldset>
                 <legend>Misc Experience</legend>
-                <ul>
+                <ul className='misc'>
                     {
                         misc.map((experience) => {
                             return (
                                 <li key={experience.id}>
-                                    <label htmlFor={`organization_${experience.id}`}>Organization:</label>
-                                    <input type='text' id={`organization_${experience.id}`} className='misc' placeholder={experience.organization} onChange={handleChange}></input>
-                                    <label htmlFor={`position_${experience.id}`}>Position:</label>
-                                    <input type='text' id={`position_${experience.id}`} className='misc' placeholder={experience.position} onChange={handleChange}></input>
-                                    <label htmlFor={`start_${experience.id}`}>Start Date:</label>
-                                    <input type='date' id={`start_${experience.id}`} className='misc' placeholder={experience.start} onChange={handleChange}></input>
-                                    <label htmlFor={`end_${experience.id}`}>End Date:</label>
-                                    <input type='date' id={`end_${experience.id}`} className='misc' placeholder={experience.end} onChange={handleChange}></input>
-                                    <label htmlFor={`description_${experience.id}`}>Description:</label>
-                                    <textarea id={`description_${experience.id}`} className='misc' placeholder={experience.description} onChange={handleChange}></textarea>
+                                    <div>
+                                        <div className="organization">
+                                            <label htmlFor={`organization_${experience.id}`}>Organization:</label>
+                                            <input type='text' id={`organization_${experience.id}`} className='misc' placeholder={experience.organization} onChange={handleChange}></input>
+                                        </div>
+                                        <div className="miscPosition">
+                                            <label htmlFor={`position_${experience.id}`}>Position:</label>
+                                            <input type='text' id={`position_${experience.id}`} className='misc' placeholder={experience.position} onChange={handleChange}></input>
+                                        </div>
+                                    </div>
+                                    <div>
+                                        <div className='miscStart'>
+                                            <label htmlFor={`start_${experience.id}`}>Start Date:</label>
+                                            <input type='date' id={`start_${experience.id}`} className='misc' placeholder={experience.start} onChange={handleChange}></input>
+                                        </div>
+                                        <div className='miscEnd'>
+                                            <label htmlFor={`end_${experience.id}`}>End Date:</label>
+                                            <input type='date' id={`end_${experience.id}`} className='misc' placeholder={experience.end} onChange={handleChange}></input>
+                                        </div>
+                                    </div>
+                                    <div className='miscDescription'>
+                                        <label htmlFor={`description_${experience.id}`}>Description:</label>
+                                        <textarea id={`description_${experience.id}`} className='misc' placeholder={experience.description} onChange={handleChange}></textarea>
+                                    </div>
                                     <button type='button' id={experience.id} className='misc' onClick={removeExperience}>Delete</button>
                                 </li>
                             );
