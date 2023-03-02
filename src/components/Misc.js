576cae7785e40a693d7b25e32bd068e2a97687cc
James Olsen <olsenjames1116@gmail.com>
2023-03-01 18:14:28 -0800

    Format misc section of resume

--- src/components/Misc.js
+++ src/components/Misc.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Misc.css';
 
 export default class Misc extends React.Component {
     constructor(props) {
@@ -9,14 +10,14 @@ export default class Misc extends React.Component {
         const { misc } = this.props;
 
         return (
-            <div>
-                <h2>Miscellaneous Experience</h2>
+            <div className='resumeMisc'>
+                <h3>Miscellaneous Experience</h3>
                 <ul>
                     {
                         misc.map((experience) => {
                             return (
                                 <li key={experience.id}>
-                                    <h3>{experience.organization}</h3>
+                                    <h4>{experience.organization}</h4>
                                     <span> - {experience.position}<br /></span>
                                     <span>{experience.start} - {experience.end}</span>
                                     <p>{experience.description}</p>
