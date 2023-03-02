7aa691c95e9421e8a06488a5ab1f3a0e2a02bc90
James Olsen <olsenjames1116@gmail.com>
2023-03-01 18:11:26 -0800

    Format education section of resume

--- src/components/Education.js
+++ src/components/Education.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../styles/Education.css';
 
 export default class Education extends React.Component {
     constructor(props) {
@@ -9,14 +10,14 @@ export default class Education extends React.Component {
         const { education } = this.props;
 
         return (
-            <div>
-                <h2>Education</h2>
+            <div className='resumeEducation'>
+                <h3>Education</h3>
                 <ul>
                     {
                         education.map((program) => {
                             return (
                                 <li key={program.id}>
-                                    <h3>{program.school}</h3>
+                                    <h4>{program.school}</h4>
                                     <span> - {program.curriculum}<br /></span>
                                     <span>{program.start} - {program.end}</span>
                                     <p>{program.description}</p>
